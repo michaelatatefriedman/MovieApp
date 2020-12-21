d132aebbfb57c43648ff58d99221c380039b9e16
Michaela Friedman <michaelatatefriedman@gmail.com>
2020-12-20 21:59:26 -0500

    beggining stages

---
+++ movie-app/src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+
+// App.js holds the state for the app
+// that was we can keep everything organized in one place
+// and pass different pieces of state to different components.
+  // state object to hold our list og movies
+  // comes from the API
+  const App = () => {
+    const [movies, setMovies] = useState([]);
+    const[searchValue, setSearchValue] = useState(' ');
+    const getMovieRequest = async (searchValue) => {
+      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+      const response = await fetch(url);
+      const responseJson = await response.json();
+
+      console.log(responseJson);
+      if(responseJson.Search){
+        setMovies(responseJson.Search);
+      }
+    };
+
+    useEffect(() => {
+      getMovieRequest(searchValue);
+    }, [searchValue]);
+    return (
+      <div className='container-fluid movie-app'>
+        <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHeading heading='Movies'/>
+          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        </div>
+        <div className="row">
+            <MovieList movies = {movies} />
+          </div>
+        </div>
+    );
+  };
+
+export default App;
